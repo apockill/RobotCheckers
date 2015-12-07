c1ba7dba49aea5f8ec86f962b2ec941201477cec
Alex Thiel <apocthiel@gmail.com>
2015-12-06 19:14:21 -0700

    More lighting issues will now auto-callibrate

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    console=['Main.py'],
+    name='RobotCheckers',
+    version='',
+    packages=['RobotArm', 'RobotArm.Tests', 'RobotArm.Video'],
+    url='',
+    license='',
+    author='AlexThiel',
+    author_email='',
+    description=''
+)
